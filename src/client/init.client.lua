108b4ae84b6faebd8fdc471437a1580bc7a15e05
Mads / Eye <eyedevz@gmail.com>
2023-03-28 20:20:20 +0200

    remove test notification that didnt work

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -3,8 +3,5 @@ local RS = game:GetService("ReplicatedStorage")
 
 local Fusion = require(RS.modules.Fusion)
 
-game:GetService("StarterGui"):SetCore("SendNotification",{
-	Title = "Basic test notification on spawn to see what this file does lol", -- Required
-	Text = "Hey!", -- Required
-	Icon = "rbxassetid://1234567890" -- Optional
-})
+
+print("test hello world")
