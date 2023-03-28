c2d9b932f0ed594167f957f1ffd10ceb2558e8bc
Mads / Eye <eyedevz@gmail.com>
2023-03-28 20:18:52 +0200

    A test notification will remove dw

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -3,3 +3,8 @@ local RS = game:GetService("ReplicatedStorage")
 
 local Fusion = require(RS.modules.Fusion)
 
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Basic test notification on spawn to see what this file does lol", -- Required
+	Text = "Hey!", -- Required
+	Icon = "rbxassetid://1234567890" -- Optional
+})
