5b0e262bb56ed707a6ee3b94e4300ab013de8ccf
Eric Apostal <ehapostal04@gmail.com>
2023-04-04 20:54:24 -0400

    fixed all jitter / shake

--- src/server/game_handler/train/train_movement.lua
+++ src/server/game_handler/train/train_movement.lua
@@ -6,67 +6,11 @@ local train_model = game.Workspace:WaitForChild("train").train
     Movement
 ]]
 
-local speed = 100
-local cart_dist = 10
+local speed = 45
+local cart_dist = 50
 
 local train = {}
 train.__index = train
-
-function train:move_to_position(pos, parent_cart)
-    --[[
-        Moves cart to a given Vector3
-        Parent cart is for any cart that has children
-    ]]
-    
-    local goal = {}
-    local _cf = CFrame.new(self.cart_object.PrimaryPart.Position, pos)
-    local rads = Vector3.new(_cf:ToEulerAnglesXYZ())
-
-    goal.CFrame = CFrame.new(pos) * CFrame.Angles(rads.X, rads.Y, rads.Z)
-
-    local p1 = self.cart_object.PrimaryPart.Position
-    local p2 = pos
-    local dist = math.abs(p1.X-p2.X) + math.abs(p1.Y-p2.Y) + math.abs(p1.Z-p2.Z)
-
-    local wait_duration = dist/speed
-
-    if parent_cart then
-        local modifier = 1
-        local magnitude = math.round((self:front_hitch().Position - parent_cart:rear_hitch().Position).Magnitude)
-        
-        if magnitude > cart_dist then
-            modifier = 0.9
-        elseif magnitude < cart_dist then
-            modifier = 1.1
-        end
-
-        wait_duration = (parent_cart.child_speed) * modifier
-    end
-
-    local tweenInfo = TweenInfo.new(wait_duration, Enum.EasingStyle.Linear, Enum.EasingDirection.Out)
-    local tween = TweenService:Create(self.cart_object.PrimaryPart, tweenInfo, goal)
-
-    --[[
-
-    When restarting the loop it needs the really small duration in order to actually move forward
-    I'm always setting the child speed to the parent speed, thus this would never work
-    What I need to do is either:
-
-    A) Figure out why there is a really small tween in the first place
-    B) Just tween the child cart manually if it's really small
-
-    ]]
-
-    if (wait_duration) and (wait_duration > 0.01) then
-        tween:Play()
-        self.child_speed = wait_duration
-        wait(wait_duration)
-    else
-        print("0 node tween!")
-        -- just in case the tween is 0, otherwise it will probably crash (usually only happens if I mess something else up)
-        wait()
-    end
-end
 
 function train:move_to_positions(positions)
     for k, v in ipairs(positions) do
@@ -87,6 +31,63 @@ function train:move_to_nodes(nodes, parent_cart)
         self:move_to_position(v.Position, parent_cart)
     end
 end
+
+function smoother(x)
+    return math.tanh(x)
+end
+
+function train:move_to_position(pos, parent_cart)
+    --[[
+        Moves cart to a given Vector3
+        Parent cart is for any cart that has children
+    ]]
+    
+    local goal = {}
+    local _cf = CFrame.new(self.cart_object.PrimaryPart.Position, pos)
+    local rads = Vector3.new(_cf:ToEulerAnglesXYZ())
+
+    goal.CFrame = CFrame.new(pos) * CFrame.Angles(rads.X, rads.Y, rads.Z)
+
+    local p1 = self.cart_object.PrimaryPart.Position
+    local p2 = pos
+    local dist = math.abs(p1.X-p2.X) + math.abs(p1.Y-p2.Y) + math.abs(p1.Z-p2.Z)
+
+    local wait_duration = dist/speed
+
+    if parent_cart then
+        local magnitude = (self.cart_object.base.Position - parent_cart.cart_object.base.Position).Magnitude
+        local dist_error = cart_dist - magnitude
+        
+        -- Set the sensitivity of the tanh function using a scaling factor (adjust this value as needed)
+        local scaling_factor = 0.1
+        -- Set an offset to control the minimum value of the modifier (adjust this value as needed)
+        local offset = 0.1
+        local modifier = 1 + smoother(scaling_factor * dist_error) * offset
+
+        wait_duration = (parent_cart.child_speed) * modifier
+
+    end
+
+    local tweenInfo = TweenInfo.new(wait_duration, Enum.EasingStyle.Linear, Enum.EasingDirection.Out)
+    local tween = TweenService:Create(self.cart_object.PrimaryPart, tweenInfo, goal)
+
+    if (wait_duration) and (wait_duration > 0.0000001) then
+        tween:Play()
+        self.child_speed = wait_duration
+
+        --[[[
+            BruhhhH nah I don't even know anymore
+
+            this WAIT STATEMENT was the issue. Literally like bruh. It would jitter because it would wait slightly too long
+        ]]
+        wait(wait_duration*.8)
+    else
+        print("0 node tween!")
+        -- just in case the tween is 0, otherwise it will probably crash (usually only happens if I mess something else up)
+        wait()
+    end
+end
+
 
 function train:set_anchor(bool)
     self.cart_object.base.Anchored = bool
